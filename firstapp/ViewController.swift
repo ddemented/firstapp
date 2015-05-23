f8c24eb3fbdbfc8e3e5aa65ba135d15842a092ac
Sanjeev S Nair <sanjeevsureshn@gmail.com>
2015-05-23 20:25:03 +0300

    Initial Commit

---
+++ firstapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firstapp
+//
+//  Created by Sanjeev Suresh on 5/23/15.
+//  Copyright (c) 2015 Sanjeev Suresh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
